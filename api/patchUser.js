40513a34bcadf1252d881a81ee6be1cf545716b7
Matt Fowler <mbfowler@gmail.com>
2023-11-07 15:31:34 -0500

    Correct knexfile path

--- api/patchUser.js
+++ api/patchUser.js
@@ -3,7 +3,7 @@ const { configDotenv } = require("dotenv")
 configDotenv()
 
 const environment = process.env.NODE_ENV || 'development'
-const configuration = require('./knexfile')[environment]
+const configuration = require('../knexfile')[environment]
 const db = require('knex')(configuration)
 
 module.exports = async (req, res) => {
