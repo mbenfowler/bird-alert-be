0418493d63b3418557febe2ed1a763902de6c21a
Matt Fowler <mbfowler@gmail.com>
2023-11-07 14:28:18 -0500

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DEV_PG_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.PROD_PG_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/dev'
+    },
+    useNullAsDefault: true
+  }
+};
