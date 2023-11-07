43b1e253c454d10996286d004e12a49094f46bb5
Matt Fowler <mbfowler@gmail.com>
2023-11-07 16:00:53 -0500

    Add migrations and seeds folder and content

---
+++ db/migrations/20231104174720_initial.js
@@ -0,0 +1,52 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('username');
+      table.string('password');
+      table.string('email');
+      table.string('phone');
+      table.string('location');
+      table.string('state');
+      table.timestamps(true, true);
+    })
+    .createTable('birds', (table) => {
+      table.increments('id').primary();
+      table.string('comName');
+      table.string('sciName');
+      table.string('speciesCode').unique();
+      table.string('category');
+      table.string('order');
+      table.string('familyCode');
+      table.string('familyComName');
+      table.string('familySciName');
+      table.string('wikiURL');
+      table.integer('taxonOrder');
+      table.timestamps(true, true);
+    })
+    .createTable('saved_birds', (table) => {
+      table.increments('id').primary();
+      table.string('speciesCode').unsigned();
+      table.integer('user_id').unsigned();
+      table.integer('bird_id').unsigned();
+      table.foreign('speciesCode').references('birds.speciesCode');
+      table.foreign('user_id').references('users.id');
+      table.foreign('bird_id').references('birds.id');
+      table.timestamps(true, true);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('saved_birds')
+    .dropTable('birds')
+    .dropTable('users')
+};
---
+++ db/seeds/dev/users.js
@@ -0,0 +1,42 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  try {
+    await knex('saved_birds').del()
+    await knex('birds').del()
+    await knex('users').del()
+
+    const [userID] = await knex('users').insert({
+      username: '',
+      password: '',
+      email: 'robinware456@gmail.com',
+      phone: '555-555-5555',
+      location: 'US-GA-139',
+      state: ''
+    }, 'id')
+
+    const [birdID] = await knex('birds').insert({
+      comName: 'Snow Goose',
+      sciName: 'Anser caerulescens',
+      speciesCode: 'snogoo',
+      category: 'species',
+      order: 'Anseriformes',
+      familyCode: 'anatid1',
+      familyComName: 'Ducks, Geese, and Waterfowl',
+      familySciName: 'Anatidae',
+      wikiURL: 'https://en.wikipedia.org/?curid=199013',
+      taxonOrder: 256
+    }, ['id', 'speciesCode'])
+
+    return knex('saved_birds').insert({
+      speciesCode: birdID.speciesCode,
+      user_id: userID.id,
+      bird_id: birdID.id
+    })
+  } catch (error) {
+    console.log(`Error seeding data: ${error}`)
+  }
+};
