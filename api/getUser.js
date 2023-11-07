ad6c20f1dea34c773a3ed52c4ff5577d46430051
Matt Fowler <mbfowler@gmail.com>
2023-11-07 15:21:52 -0500

    Update user functions with knex

--- api/getUser.js
+++ api/getUser.js
@@ -1,11 +1,16 @@
+const { knex } = require('knex')
 const { configDotenv } = require("dotenv")
 
 configDotenv()
-const db = require(env.process.PROD_PG_URL)
 
-app.get('/api/v1/user/:id', async (req, res) => {
-  const userId = req.params.id;
-  await db('users').where({id: userId}).first()
-    .then(user => res.status(200).json(user))
-    .catch(error => res.status(500).json({ error }))
-})
+const configuration = require('./knexfile')
+const db = require('knex')(configuration)
+
+module.exports = async (req, res) => {  
+  try {
+    const user = await db('users').first()
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+}
--- api/patchUser.js
+++ api/patchUser.js
@@ -1,19 +1,26 @@
+const { knex } = require('knex')
 const { configDotenv } = require("dotenv")
 
 configDotenv()
-const db = require(env.process.PROD_PG_URL)
 
-app.patch('/api/v1/user', async (req, res) => {
-  const user = req.body
-  await db('users').where('id', 1).update({
-    username: user.name,
-    password: user.password,
-    location: user.location,
-    email: user.email,
-    phone: user.phone,
-    state: user.state,
-    updated_at: new Date()
-  })
-    .then(() => res.status(200).json(user))
-    .catch(error => res.status(500).json({ error }))
-})
+const configuration = require('./knexfile')
+const db = require('knex')(configuration)
+
+module.exports = async (req, res) => {
+  const user = req.body;
+
+  try {
+    await db('users').where('id', 1).update({
+      username: user.name,
+      password: user.password,
+      location: user.location,
+      email: user.email,
+      phone: user.phone,
+      state: user.state,
+      updated_at: new Date(),
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
