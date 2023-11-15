207e065e8ff1db645242bc72f7477f49d0544748
mbfowler <mbfowler@gmail.com>
2023-11-14 19:34:15 -0500

    Simplify db interactions

--- api/createUser.js
+++ api/createUser.js
@@ -9,29 +9,16 @@ const db = require('knex')(configuration)
 module.exports = async (req, res) => {
   const { email, password } = req.query
   try {
-    console.log('Start of /api/createUser function');
-    const userQueryStartTime = Date.now();
-    const user = await db('users').where({ email }).first()
-    const userQueryDuration = Date.now() - userQueryStartTime;
-    console.log('User query duration:', userQueryDuration, 'ms');
-    console.log('Received request with email:', email);
-    if (!user) {
-      await db('users').insert({ 
-        email,
-        password,
-        username: '',
-        phone: '',
-        location: '',
-        state: ''
-      })
+    await db('users').insert({
+      email,
+      password,
+      username: '',
+      phone: '',
+      location: '',
+      state: ''
+    })
 
-      const createUserDuration = Date.now() - createUserStartTime;
-      console.log('Create user duration:', createUserDuration, 'ms');
-
-      console.log('End of /api/createUser function');
-
-      res.status(200).json({ success: true });
-    }
+    res.status(200).json({ success: true })
   } catch (error) {
     res.status(500).json({ error })
   }
