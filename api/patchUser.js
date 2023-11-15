b9d8e332971127cf04f6dd4f122bf48d207e01fd
mbfowler <mbfowler@gmail.com>
2023-11-15 11:47:27 -0500

    Comment out unneeded properties in patchUser

--- api/patchUser.js
+++ api/patchUser.js
@@ -11,13 +11,13 @@ module.exports = async (req, res) => {
   console.log(user)
   try {
     await db('users').where(user.email).update({
-      username: user.name,
-      password: user.password,
+      // username: user.name,
+      // password: user.password,
       location: user.location,
       email: user.email,
       phone: user.phone,
-      state: user.state,
-      updated_at: new Date(),
+      // state: user.state,
+      updated_at: new Date()
     });
     res.status(200).json(user);
   } catch (error) {
