1be5ac8b4282aa18cc2a58d83e11e45c1e934bf7
mbfowler <mbfowler@gmail.com>
2023-11-14 19:19:33 -0500

    Add more logging

--- api/createUser.js
+++ api/createUser.js
@@ -9,7 +9,12 @@ const db = require('knex')(configuration)
 module.exports = async (req, res) => {
   const { email, password } = req.query
   try {
+    console.log('Start of /api/createUser function');
+    const userQueryStartTime = Date.now();
     const user = await db('users').where({ email }).first()
+    const userQueryDuration = Date.now() - userQueryStartTime;
+    console.log('User query duration:', userQueryDuration, 'ms');
+    console.log('Received request with email:', email);
     if (!user) {
       await db('users').insert({ 
         email,
@@ -19,6 +24,13 @@ module.exports = async (req, res) => {
         location: '',
         state: ''
       })
+
+      const createUserDuration = Date.now() - createUserStartTime;
+      console.log('Create user duration:', createUserDuration, 'ms');
+
+      console.log('End of /api/createUser function');
+
+      res.status(200).json({ success: true });
     }
   } catch (error) {
     res.status(500).json({ error })
