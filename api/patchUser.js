0a4d1cd9236ddbe8791d814b120a43b3bc248b8a
Matt Fowler <mbfowler@gmail.com>
2023-12-22 19:24:02 -0500

    Update patchUser for notifications

--- api/patchUser.js
+++ api/patchUser.js
@@ -8,7 +8,6 @@ const db = require('knex')(configuration)
 
 module.exports = async (req, res) => {
   const user = req.query;
-  
   try {
     const updateUser = {
       email: user.email,
@@ -19,8 +18,12 @@ module.exports = async (req, res) => {
     user.location && (updateUser.location = user.location)
     const confirmedBool = user.confirmed === 'true' ? true : false
     user.confirmed && (updateUser.email_confirmed = confirmedBool)
-    user.alert_digest_email_enabled && (updateUser.alert_digest_email_enabled = user.alert_digest_email_enabled)
-    user.rare_sightings_email_enabled && (updateUser.rare_sightings_email_enabled = user.rare_sightings_email_enabled)
+
+    const alertDigestBool = user.alert_digest_email_enabled === 'true' ? true : false
+    user.alert_digest_email_enabled && (updateUser.alert_digest_email_enabled = alertDigestBool)
+
+    const rareSightingsBool = user.rare_sightings_email_enabled === 'true' ? true : false
+    user.rare_sightings_email_enabled && (updateUser.rare_sightings_email_enabled = rareSightingsBool)
     user.password && (updateUser.password = user.password)
     user.phone && (updateUser.phone = user.phone)
 
